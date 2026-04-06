bb00d879b3a3758027cb7b2a8c933440991e38db
User <annabeat08@gmail.com>
2026-04-06 18:09:54 +0300

    Creating a dict with fixed data

---
+++ DAY12_Exercise 30.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+
+    person = {
+        "name": "Alice",
+        "age": 25,
+        "country": "Kenya"
+    }
+
+    print(f"{person}")
+
+if __name__ == "__main__":
+    sys.exit(main())
