225b2555892a092bb605be0d59946c83f90e2c9f
User <annabeat08@gmail.com>
2026-04-21 12:27:37 +0300

    Creating a dict using the user input data from the list

--- Day14_Exercise36.py
+++ Day14_Exercise36.py
@@ -2,15 +2,19 @@ import sys
 
 def main():
 
-    student = input("Name: ")
+    name = input("Name: ")
     score1 = int(input("Enter first score: "))
     score2 = int(input("Enter second score: "))
     score3 = int(input("Enter third score: "))
 
     score = [score1, score2,score3]
 
-
     print(f"{score}")
+
+    student = {"StudentName":name, "ListOfScore":score}
+
+    print(f"{student}")
+
 
 if __name__ == "__main__":
     sys.exit(main())
