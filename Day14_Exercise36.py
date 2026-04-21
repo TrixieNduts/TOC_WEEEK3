1990300ded7f70c6bb60f2164b502581b8226635
User <annabeat08@gmail.com>
2026-04-21 12:41:18 +0300

    Getting the average of our score

--- Day14_Exercise36.py
+++ Day14_Exercise36.py
@@ -2,6 +2,7 @@ import sys
 
 def main():
 
+    #part 1
     name = input("Name: ")#askig for user input
     score1 = int(input("Enter first score: "))#asking user for input and converting to integer
     score2 = int(input("Enter second score: "))#asking user for input and converting to integer
@@ -11,9 +12,15 @@ def main():
 
     print(f"{score}")#thid prints the list
 
+    #part 2
     student = {"StudentName":name, "ListOfScore":score}#creating a dict from the user input data using keys
 
     print(f"{student}")#this prints the dict
+
+    #part 3
+    averagescore = sum(score)/3
+
+    print(f"{averagescore}")
 
 
 if __name__ == "__main__":
